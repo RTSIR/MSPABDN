ab785ac32affa156988b6f6eb23643b1df7f3014
RAMESH KUMAR THAKUR <89151608+RTSIR@users.noreply.github.com>
2023-06-15 17:32:01 +0530

    Update MSPABDN_Gray.py

--- MSPABDN_Gray.py
+++ MSPABDN_Gray.py
@@ -218,4 +218,4 @@ model.fit_generator(myFlow(aug,cleanImages),
 epochs=config.epochs,steps_per_epoch=len(cleanImages)//config.batch_size,callbacks=callbacks,verbose=1)
 
 # save the model
-model.save('MSPABDN_Gray.h5')
+model.save('./Pretrained_models/MSPABDN_Gray.h5')
